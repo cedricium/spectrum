c885a82f5cf0b20d48cb94a3730171393c8718c6
Brian Lovin <briandlovin@gmail.com>
2018-04-20 07:28:16 -0700

    Small changes to admin dash grid layout

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router';
+import { Router, Route, Switch } from 'react-router';
 import styled from 'styled-components';
 import { history } from './helpers/history';
 import ModalRoot from './components/modals/modalRoot';
@@ -8,13 +8,13 @@ import Users from './views/users';
 import Threads from './views/threads';
 import Communities from './views/communities';
 import Navbar from './views/navbar';
-import Dashboard from './views/dashboard';
+import Home from './views/home';
 import PrivateRoute from './utils/routeAuth';
 
 const Body = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 100vw;
+  width: 100%;
+  max-width: 100%;
   height: 100vh;
   overflow-y: scroll;
   background: ${props => props.theme.bg.wash};
@@ -22,7 +22,16 @@ const Body = styled.div`
   @media (max-width: 768px) {
     height: 100vh;
     max-height: ${window.innerHeight}px;
+    flex-direction: column;
   }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex: auto;
+  flex-wrap: wrap;
+  overflow-y: scroll;
+  padding: 16px;
 `;
 
 class Routes extends React.Component<Props, State> {
@@ -33,14 +42,17 @@ class Routes extends React.Component<Props, State> {
           <Route component={Navbar} />
           <Route component={ModalRoot} />
           <Route component={Toasts} />
-          <Switch>
-            <PrivateRoute exact path="/" component={Dashboard} />
-            <PrivateRoute path="/users/:username" component={Users} />
-            <PrivateRoute path="/users" component={Users} />
-            <PrivateRoute path="/communities/:slug" component={Communities} />
-            <PrivateRoute path="/communities" component={Communities} />
-            <PrivateRoute path="/threads" component={Threads} />
-          </Switch>
+
+          <ContentContainer>
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+              <PrivateRoute path="/users/:username" component={Users} />
+              <PrivateRoute path="/users" component={Users} />
+              <PrivateRoute path="/communities/:slug" component={Communities} />
+              <PrivateRoute path="/communities" component={Communities} />
+              <PrivateRoute path="/threads" component={Threads} />
+            </Switch>
+          </ContentContainer>
         </Body>
       </Router>
     );
