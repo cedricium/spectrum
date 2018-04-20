5e8061f340ba6d834c9a5012d46e3de1cda46a6d
Brian Lovin <briandlovin@gmail.com>
2018-04-20 07:30:18 -0700

    One more root admin query resolver

---
+++ api/queries/admin/totalUsers.js
@@ -0,0 +1,10 @@
+// @flow
+import type { GraphQLContext } from '../../';
+import { isAdmin } from '../../utils/permissions';
+import { getCount } from '../../models/utils';
+
+export default async (_: any, __: any, { user }: GraphQLContext) => {
+  if (!isAdmin(user.id)) return null;
+
+  return await getCount('users');
+};
